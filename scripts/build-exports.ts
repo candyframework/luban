830dfda30ee40f56c6ae34dc0d5aaf9d60590ffa
Leo <afuafu@bitlightlabs.com>
2024-12-13 20:08:07 +0800

    feat: add exports

---
+++ scripts/build-exports.ts
@@ -0,0 +1,24 @@
+const tree: Record<string, string> = {
+    '.': './src/mod.ts',
+};
+
+export default async function build(dir: string) {
+    const entries = Deno.readDir(dir);
+    for await (const entry of entries) {
+        if (entry.name === '.' || entry.name === '..' || entry.name === '.DS_Store') {
+            continue;
+        }
+
+        const full = dir + '/' + entry.name;
+        if (entry.isFile) {
+            const key = full.replace('/src/', '/');
+            tree[key] = full;
+        } else if (entry.isDirectory) {
+            await build(full);
+        }
+    }
+}
+
+build('./src').then(() => {
+    console.log(tree);
+});
